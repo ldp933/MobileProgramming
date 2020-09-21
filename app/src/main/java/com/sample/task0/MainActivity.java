59437fbe1df472b185c6c3e783312f8931953483
lina dwi pratiwi <pratiwilinadwi@gmail.com>
2020-09-21 08:29:50 +0700

    runnable

--- app/src/main/java/com/sample/task0/MainActivity.java
+++ app/src/main/java/com/sample/task0/MainActivity.java
@@ -1,13 +1,52 @@
 package com.sample.task0;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+   List<String> toDoList;
+   ArrayAdapter<String> arrayAdapter;
+   ListView listView;
+
+   EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toDoList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.list_text_view, toDoList);
+
+        listView = findViewById(R.id.list_view);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                TextView textView = (TextView) view;
+                textView.setPaintFlags(textView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+            }
+        });
+
+        editText = findViewById(R.id.edit_text);
+    }
+
+    public void addItemToList(View view){
+        toDoList.add(editText.getText().toString());
+        arrayAdapter.notifyDataSetChanged();
+        editText.setText("");
     }
 }
